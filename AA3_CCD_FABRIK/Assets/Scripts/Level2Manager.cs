3b4ee8dd217dc8875052b7c40c02b098b78412c1
JoanaMorenoC <juana.moreno@enti.cat>
2026-01-08 18:37:55 +0100

    Make buttons deactivate lasers and put barrier before winning

--- AA3_CCD_FABRIK/Assets/Scripts/Level2Manager.cs
+++ AA3_CCD_FABRIK/Assets/Scripts/Level2Manager.cs
@@ -5,17 +5,10 @@ using UnityEngine;
 public class Level2Manager : MonoBehaviour
 {
     [SerializeField] private List<ButtonHolder> buttons;
+    [SerializeField] private List<GameObject> lasers;
     [SerializeField] private FABRIKSolver solver;
     [SerializeField] private Transform defaultTarget;
-    [SerializeField] private GameObject winningArea;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         CheckButtons();
@@ -32,8 +25,18 @@ public class Level2Manager : MonoBehaviour
             }
         }
 
+        ActivateVictory();
+    }
+
+    void ActivateVictory()
+    {
         ChangeTarget(defaultTarget);
-        winningArea.SetActive(true);
+
+        for (int i = 0; i < lasers.Count; i++)
+        {
+            lasers[i].SetActive(false);
+        }
+
     }
 
     void ChangeTarget(Transform newTarget)
